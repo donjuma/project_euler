bade8ef7dd97b6cb54f6a6375515ba58ceed3415
Omar Juma <Omar.X.Juma.-ND@disney.com>
2018-02-27 14:12:27 -0800

    Problem 10: removed old code

--- python/10/summation_of_primes.py
+++ python/10/summation_of_primes.py
@@ -1,27 +1,6 @@
 import math
 MAX_PRIMES = 2000000
 
-#def list_factors(x):
-#    factors = []
-#    d = 2
-#    while (x > 1):
-#        while (x % d == 0):
-#            factors.append(d)
-#            x /= d
-#        d += 1
-#    return factors
-#
-#
-#def find_primes(n):
-#    primes = []
-#    for i in range(n):
-#        if (i != 2) and (i % 2 != 0):
-#            if len(list_factors(i)) == 1:
-#                primes.append(i)
-#    return primes
-#
-#print sum(find_primes(MAX_PRIMES))
-
 def isPrime(num):
     if num < 2:
         return False
