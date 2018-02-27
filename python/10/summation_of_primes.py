4c28976ba3a1cfbddd6698cc34794ac38ddd4b11
Omar Juma <Omar.X.Juma.-ND@disney.com>
2018-02-27 14:05:12 -0800

    Finished problem 10

---
+++ python/10/summation_of_primes.py
@@ -0,0 +1,38 @@
+import math
+MAX_PRIMES = 2000000
+
+#def list_factors(x):
+#    factors = []
+#    d = 2
+#    while (x > 1):
+#        while (x % d == 0):
+#            factors.append(d)
+#            x /= d
+#        d += 1
+#    return factors
+#
+#
+#def find_primes(n):
+#    primes = []
+#    for i in range(n):
+#        if (i != 2) and (i % 2 != 0):
+#            if len(list_factors(i)) == 1:
+#                primes.append(i)
+#    return primes
+#
+#print sum(find_primes(MAX_PRIMES))
+
+def isPrime(num):
+    if num < 2:
+        return False
+
+    for i in range(2, int(math.sqrt(num)) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+count = 0
+for i in range(2000000):
+    if isPrime(i):
+        count += i
+print count
